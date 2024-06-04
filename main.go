535b53116df0dc4042b7f73f0f37cf16ce42208a
nemopss <alexey_gladilin@mail.ru>
2024-06-04 04:04:21 +0300

    Added TODO

--- main.go
+++ main.go
@@ -11,3 +11,13 @@ func main() {
 
 	fmt.Println(*urlflag)
 }
+
+/*
+	TODO:
+	1. GET the webpage
+	2. Parse all the links on the page
+	3. Build proper urls with our links
+	4. Filter out any links with a different domain
+	5. Find all pages (BFS)
+	6. Print out XML
+*/
